6c369a7e32c561c67f82da30e8db65858e6080d0
fortwoone <denauxarthur@gmail.com>
2023-12-02 21:14:02 +0100

    Will this work?

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ import sys, os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'PyZora'
 copyright = '2023, fortwoone'
